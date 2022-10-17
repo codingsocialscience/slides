e6338c87476982aa1dab9c7f2a3c7e849ae3ff5a
Matti Nelimarkka <matti.nelimarkka@helsinki.fi>
2022-10-17 11:30:14 +0300

    Fix figure to PNG script

--- update_figures.sh
+++ update_figures.sh
@@ -3,3 +3,11 @@ shopt -s nullglob
 for pdf in *{pdf,PDF} ; do
     convert -density 1500 -quality 100 "$pdf" "${pdf%%.*}.png"
 done
+cd ..
+
+cd figures/
+shopt -s nullglob
+for pdf in *{pdf,PDF} ; do
+    convert -density 1500 -quality 100 "$pdf" "${pdf%%.*}.png"
+done
+cd ..
